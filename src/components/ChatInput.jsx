4eb08c335334cc32cce2ba1b8e66b59f3c50296a
Marvin Brown <marvin@the-firebrand.com>
2023-08-10 15:21:11 -0500

    added comments to ChatInput component and adjusted send icon color

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -5,6 +5,7 @@ import { FaPaperPlane } from "react-icons/fa";
 const ChatInput = ({ onSubmit }) => {
   const [input, setInput] = useState("");
 
+  //submits the users chat input upon pressing enter
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       onSubmit(input);
@@ -13,6 +14,7 @@ const ChatInput = ({ onSubmit }) => {
   };
 
   return (
+    //returns the chat input at 100% width within its parent container
     <InputGroup w="100%" zIndex={1}>
       <Input
         onChange={(event) => {
@@ -27,6 +29,7 @@ const ChatInput = ({ onSubmit }) => {
       ></Input>
       <InputRightElement>
         <FaPaperPlane
+          color="white"
           onClick={() => {
             onSubmit(input);
             setInput("");
