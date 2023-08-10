84a71938a47e59648b9e0c0dcd0ace3e78b2fc97
Marvin Brown <marvin@the-firebrand.com>
2023-08-10 15:25:08 -0500

    cleaned up chatTurbo module and added useful comments

--- src/modules/chatTurbo.js
+++ src/modules/chatTurbo.js
@@ -1,39 +1,42 @@
 import axios from "axios";
 
 function chatTurbo(system, chat) {
-    const config = {
-      organization: "org-jXq1jagmrAh2SvVmFUvvOHlj",
-      apiKey: "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",
-      endpoint: "https://api.openai.com/v1/chat/completions",
-    };
-  
-    return new Promise((resolve, reject) => {  
-        system = {"role": "system", "content": system}
-        // chat.unshift(system)
+  //open api configuration
+  //TO DO: hide this within firebase functions variable to protect api key
+  const config = {
+    organization: "org-jXq1jagmrAh2SvVmFUvvOHlj",
+    apiKey: "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",
+    endpoint: "https://api.openai.com/v1/chat/completions",
+  };
 
-        axios
-          .post(
-            config.endpoint,
-            {
-              model: 'gpt-3.5-turbo',
-              messages: [system, ...chat],
-              max_tokens: 2000,
-              n: 1,
-              temperature: 1.1,
-            },
-            {
-              headers: {
-                Authorization: `Bearer ${config.apiKey}`,
-              },
-            }
-          )
-          .then((res) => {
-            resolve(res.data.choices[0].message.content);
-          })
-          .catch((err) => {
-            reject(err);
-          })
-        })
-    }
+  return new Promise((resolve, reject) => {
+    //creating system object for insertion in post request below
+    system = { role: "system", content: system };
 
- export default chatTurbo;   
+    //spreads the existing chat log history into the post request and returns the gpt response
+    axios
+      .post(
+        config.endpoint,
+        {
+          model: "gpt-3.5-turbo",
+          messages: [system, ...chat],
+          max_tokens: 2000,
+          n: 1,
+          temperature: 1.1,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${config.apiKey}`,
+          },
+        }
+      )
+      .then((res) => {
+        resolve(res.data.choices[0].message.content);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+export default chatTurbo;
